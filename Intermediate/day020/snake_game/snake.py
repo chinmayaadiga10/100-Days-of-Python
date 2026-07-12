512f30079bd32d00c79a044404e17f8e28a61ee1
Chinmaya Adiga <chinmayaadiga.sf@gmail.com>
2026-07-12 12:08:53 +0530

    creating snake for game

---
+++ Intermediate/day020/snake_game/snake.py
@@ -0,0 +1,68 @@
+#snake class creates the snake 
+
+#importing turtle module
+from turtle import Turtle
+
+STARTING_POSITIONS=[(0,0),(-20,0),(-40,0)]
+MOVE_DISTANCE=20
+UP=90
+DOWN=270
+RIGHT=0
+LEFT=180
+
+class Snake:
+    def __init__(self):
+        self.segments=[]
+        self.create_snake()
+        self.head=self.segments[0]
+        
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            self.add_segment(position=position)
+            
+            
+    def move(self):
+        for segment_number in range(len(self.segments)-1,0,-1):
+            new_x=self.segments[segment_number-1].xcor()
+            new_y=self.segments[segment_number-1].ycor()
+            self.segments[segment_number].goto(new_x,new_y)
+        self.segments[0].forward(MOVE_DISTANCE)
+        
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000,1000)  
+        self.segments.clear()
+        self.create_snake()
+        self.head=self.segments[0]
+        
+    
+    #this function is used to add create snake 
+    def add_segment(self,position):
+        new_segment=Turtle(shape="square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+        
+        
+    #adds a new segment to the snake to make it longer as it keeps eating food
+    #used whenever we are extending length of snake
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+        
+        
+    def up(self):
+        if self.head.heading()!=DOWN:
+            self.head.setheading(UP)
+    def left(self):
+        if self.head.heading()!=RIGHT:
+            self.head.setheading(LEFT)
+    def right(self):
+        if self.head.heading()!=LEFT:
+            self.head.setheading(RIGHT)
+    def down(self):
+        if self.head.heading()!=UP:
+            self.head.setheading(DOWN)
+        
+    
+        
