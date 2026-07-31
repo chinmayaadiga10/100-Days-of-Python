e7ae11582b93cb62bab72fd8d03d1af98039029d
Chinmaya Adiga <chinmayaadiga.sf@gmail.com>
2026-07-31 23:27:32 +0530

    incoroporate error handling to NATO  alphabet game

--- Intermediate/day026/nato-alphabet-game/main.py
+++ Intermediate/day026/nato-alphabet-game/main.py
@@ -1,27 +1,39 @@
 import pandas
 
-
-
 {"A": "Alfa", "B": "Bravo"}
-df=pandas.read_csv("./nato_phonetic_alphabet.csv")
+df = pandas.read_csv("./nato_phonetic_alphabet.csv")
 # print(df)
 
-new_dict={row.letter:row.code for (index,row) in df.iterrows()}
+new_dict = {row.letter: row.code for (index, row) in df.iterrows()}
 # print(new_dict)
 
-user_name=input("Enter your name : ").upper()
-    
+user_name = input("Enter your name : ").upper()
+
 # print(user_name)
-name=[name for name in user_name]
+name = [name for name in user_name]
 # print(name)
 
-nato_list=[]
+nato_list = []
 for letter in name:
-    nato_word=new_dict[f"{letter}"]
+    nato_word = new_dict[f"{letter}"]
     nato_list.append(nato_word)
 print(nato_list)
-    
 
-#using list comprehension -> 
-output_list=[new_dict[letter] for letter in user_name]
+
+# using list comprehension ->
+output_list = [new_dict[letter] for letter in user_name]
 print(output_list)
+
+
+def generate_phonetic():
+    user_name = input("Enter your name : ").upper()
+    try:
+        new_list = [new_dict[letter] for letter in user_name]
+    except KeyError:
+        print("Sorry only letters in the english alphabet please .")
+        generate_phonetic()
+    else:
+        print(new_list)
+
+
+generate_phonetic()
